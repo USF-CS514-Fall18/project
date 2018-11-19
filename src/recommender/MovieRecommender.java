2ae8a9339002f57359ca3a144fec27bab953ba83
Olga Karpenko <okarpenko@usfca.edu>
2018-11-19 11:07:02 -0800

    Skeleton code for the project.

---
+++ src/recommender/MovieRecommender.java
@@ -0,0 +1,6 @@
+package recommender;
+
+public class MovieRecommender {
+    // Your movie recommender code: FILL IN CODE
+    // Add other classes as needed
+}
---
+++ src/server/RecommenderServer.java
@@ -0,0 +1,8 @@
+package server;
+
+/** Jetty server for the project. Your servlets should be in this package as well
+ *  */
+public class RecommenderServer {
+        // FILL IN CODE
+        // Add other classes to this package as needed
+}
